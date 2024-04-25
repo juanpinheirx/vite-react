c581d11b06e7ab84f1a330b4a6ef93357c6d9229
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-04-25 17:52:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Academia Áries</h1>
+        </div>
+        <div>
+          <h2>Transforme seu suor em sucesso.</h2>
+        </div>
+        <div>
+          <button>Equipamentos</button>
+          <button>Planos</button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
